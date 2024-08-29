4dbb604991d6347f929eabb1cf2da40e33887ced
Your Name <you@example.com>
2024-08-29 18:32:06 +0500

    first commit

---
+++ app/(root)/_components/navbar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+// import ModeToogle from "@/components/shared/mode-toogle"
+// import { navLink } from "@/constants"
+import Link from "next/link"
+// import GlobalSearch from "./global-search"
+import {  usePathname } from "next/navigation"
+import { cn } from "@/lib/utils"
+import { navLink } from "../(home)/constants"
+import ModeToogle from "@/components/shared/mode-toggle"
+// import Mobile from "./mobile"
+
+const Navbar = () => {
+    const pathName = usePathname()
+
+    
+  return (
+    <div className='h-[10vh] backdrop:blur-sm border-b fixed z-40 inset-0 bg-background'>
+    <div className='container max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between'>
+       <Link href={'/'}>
+        <h1 className='text-4xl font-creteRound'>Smartmiz</h1>
+       </Link>
+       <div className='gap-2 hidden md:flex'>
+       {navLink.map(nav =>(
+        <Link key={nav.route} href={nav.route}
+        className={cn(
+          'hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors',
+          pathName === nav.route && 'text-blue-400'
+        )}>
+        {nav.name}
+      </Link>
+       ))}
+       </div>
+            <div className="flex items-center gap-1">
+              <ModeToogle/>
+            
+        {/* <GlobalSearch/>
+        <Mobile/> */}
+            </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+import { Contact2, FileCode2, FolderArchive, Home, ListCollapse } from "lucide-react";
+
+
+export const navLink = [
+    { name: 'Home', route: '/', icon: Home },
+    // { name: 'About', route: '/about', icon: ListCollapse },
+
+    { name: 'Contact', route: '/contact', icon: Contact2 },
+
+]
