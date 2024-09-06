701a748b98c38343e8db3971e25d5e0e225c7701
Your Name <you@example.com>
2024-09-06 19:28:28 +0500

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,30 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+
+
+
   title: "Smartmiz ",
   description: "Smartmiz o'quv markaziga hush kelibsiz",
+
+  authors: [{ name: 'Muhammad ibrohimov', url: 'https://smartmiz.uz' }],
+	icons: { icon: '/SMARTMIZ.jpg' },
+	keywords:
+		"Muhammad ibrohimov, Doniyor tohirov, dasturlash kurslari, dasturlashga oid darslar, reactjs uzbek tilida, vuejs uzbek tilida, redux uzbek tilida, smart, smartmiz o'quv markazi, ingliz tili, rus tili, cefr, smartmiz javascript, smartmiz  yorqin kelajak, javascript, reactjs, vuejs, koreys tili darslari, reactjs darslari, ielts , c1 rus tili, turk tili, topic koreys tili, rus tili o'zbek tilida, koreys tili darslari, javascript darslari o'zbek tilida, dasturash darslari o'zbek tilida, dasturlashni o'rganish, dasturlash, IT loyihalar o'zbek tilida",
+
+    openGraph: {
+      title: 'ironblog  ',
+      description:
+        "Smartmiz o'quv markaziga hush kelibsiz",
+      type: 'website',
+      url: 'https://smartmiz.uz',
+      locale: 'en_EN',
+      images: 'https://idyllic-sprite-7cad0a.netlify.app/img/bg.png',
+      countryName: 'Uzbekistan',
+      siteName: 'smartmiz',
+      emails: 'muhammadibrohimov0306@gmail.com',
+    },
+
 };
 
  function RootLayout({children}: ChildProps) {
