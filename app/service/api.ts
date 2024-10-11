8e2f61fcc03258f17d6bbf0ee4363595203075ae
Your Name <you@example.com>
2024-10-11 10:05:24 +0500

    new

--- app/service/api.ts
+++ app/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://smartmiz-2462bfd28095.herokuapp.com/',
+    baseURL: 'https://smartmiz-2462bfd28095.herokuapp.com',
     headers: {
         'Content-Type': 'application/json'
     }
