a64daa3bd9b2d444e23a3210d3773215631544de
Your Name <YOUR_EMAIL>
2024-10-13 23:45:28 +0500

    fix remonte

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,37 +2,33 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Biz haqimizda - Smartmiz o'quv markazi",
-  description: "Smartmiz o'quv markazi haqida to'liq ma'lumot. Bizning maqsadlarimiz, jamoamiz va ta'lim xizmatlarimiz haqida batafsil bilib oling.",
-}
-
+  description:
+    "Smartmiz o'quv markazi haqida to'liq ma'lumot. Bizning maqsadlarimiz, jamoamiz va ta'lim xizmatlarimiz haqida batafsil bilib oling.",
+};
 
 function About() {
-  
   return (
-    <div className="mt-28 container h-screen">
-      <h1 className="text-3xl mb-4 text-center">SMARTMIZ o‘quv markazi tarixi haqida. 2016 – 2024! </h1>
-      <p>
+    <div className="mt-28 container  h-[100%]  md:h-[71vh] mb-4 ">
+      <h1 className="text-3xl mb-4 text-center">
+        SMARTMIZ o‘quv markazi tarixi haqida. 2016 – 2024!{" "}
+      </h1>
+      <p className="text-center">
         🔥 «SMARTMIZ» o‘quv markazining 8 yillik faoliyati davomida turli
         qiyinchiliklar, jamoadagi insonlarning ketishi, to‘siqlar va muammolar
-        bo‘ldi.  
+        bo‘ldi. Shunga qaramasdan SMARTMIZ katta yutuqlarga erishdi, 11 mingdan
+        ziyod o‘quvchilarga ingliz tilidan taʼlim berdi. Koreys tili ✅ Rus tili
+        ✅ Ingliz tili ✅ Turk tili ✅ Arab tili ✅ Web dasturlash ✅ Kompyuter
+        kursi ✅ Mental arifmetika kurslari mavjud ✅ Ushbu bosib o`tilgan 9
+        yillik tarixi haqida sizlar uchun videorolik tayyorladik. o`quv
+        markazimiz haqida video ko`rish uchun link ustiga bosing
+        <a
+          target="_blank"
+          href="https://www.instagram.com/tv/CazdM-nFCUF/?igsh=eWdscjR0OXMwcHEx"
+          className="text-blue-700"
+        >
+          👉 Link 👈
+        </a>
       </p>
-      <p> Shunga qaramasdan SMARTMIZ katta yutuqlarga erishdi, 11 mingdan
-      ziyod o‘quvchilarga ingliz tilidan taʼlim berdi. </p>
-
-      <p>
-      Koreys tili ✅ 
-      Rus tili ✅
-      Ingliz tili ✅
-      Turk tili ✅
-      Arab tili ✅
-      Web dasturlash ✅
-      Kompyuter kursi ✅ 
-      Mental arifmetika kurslari mavjud ✅ </p>
-      
-      <p>  Ushbu bosib o`tilgan
-      9 yillik tarixi haqida sizlar uchun videorolik tayyorladik. </p>
-      <p>o`quv markazimiz haqida video ko`rish uchun link ustiga bosing</p>
-    <a target="_blank" href="https://www.instagram.com/tv/CazdM-nFCUF/?igsh=eWdscjR0OXMwcHEx" className="text-blue-700">👉 Link 👈</a>
     </div>
   );
 }
--- components/form/formContact.tsx
+++ components/form/formContact.tsx
@@ -1,114 +1,151 @@
-'use client'
+"use client";
 
-import { contactSchema } from '@/lib/validation'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Send } from 'lucide-react'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
-import { z } from 'zod'
-import { Button } from '../ui/button'
-import { Form, FormControl, FormField, FormItem, FormMessage } from '../ui/form'
-import { Input } from '../ui/input'
-
+import { contactSchema } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Send } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Select } from "@radix-ui/react-select";
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 function ContactForm() {
-	const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-	const form = useForm<z.infer<typeof contactSchema>>({
-		resolver: zodResolver(contactSchema),
-		defaultValues: {
-	
-			tel: '',
-			name: '',
-		},
-	})
+  const form = useForm<z.infer<typeof contactSchema>>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: {
+      tel: "",
+      name: "",
+      kurs: "",
+    },
+  });
 
-	function onSubmit(values: z.infer<typeof contactSchema>) {
-		setIsLoading(true)
-	
-		const telegramBotId = process.env.NEXT_PUBLIC_TETELGRAM_BOT_API!
-      
-		
-		const telegramChatId = process.env.NEXT_PUBLIC_TETELGRAM_CHAT_ID!
-        console.log(telegramBotId);
-        console.log(telegramChatId);
-        
-		const promise = fetch(
-			`https://api.telegram.org/bot${telegramBotId}/sendMessage`,
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-					'cache-control': 'no-cache',
-				},
-				body: JSON.stringify({
-					chat_id: telegramChatId,
-					text: `Ism: ${values.name}:
-telefon nomeri: ${values.tel}:`,
-				}),
-			}
-		)
-			.then(() => form.reset())
-			.finally(() => setIsLoading(false))
+  function onSubmit(values: z.infer<typeof contactSchema>) {
+    setIsLoading(true);
 
-		toast.promise(promise, {
-			loading: 'Loading...',
-			success: 'Successfully sent!',
-			error: 'Something went wrong!',
-		})
-	}
+    const telegramBotId = process.env.NEXT_PUBLIC_TETELGRAM_BOT_API!;
 
-	return (
-		<Form {...form}>
-			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-3'>
-			
-				<FormField
-					control={form.control}
-					name='tel'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input
-								type='number'
-									placeholder='Telefon nomer '
-									disabled={isLoading}
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<FormField
-					control={form.control}
-					name='name'
-					render={({ field }) => (
-						<FormItem>
-							<FormControl>
-								<Input
-								
-									placeholder='Ismingiz'
-									disabled={isLoading}
-									{...field}
-								/>
-							</FormControl>
-							<FormMessage />
-						</FormItem>
-					)}
-				/>
-				<Button
-					className='w-fit'
-					size={'lg'}
-					type='submit'
-					disabled={isLoading}
-				>
-					<span>Send</span>
-					<Send className='w-4 h-4 ml-2' />
-				</Button>
-			</form>
-		</Form>
-	)
+    const telegramChatId = process.env.NEXT_PUBLIC_TETELGRAM_CHAT_ID!;
+    console.log(telegramBotId);
+    console.log(telegramChatId);
+
+    const promise = fetch(
+      `https://api.telegram.org/bot${telegramBotId}/sendMessage`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "cache-control": "no-cache",
+        },
+        body: JSON.stringify({
+          chat_id: telegramChatId,
+          text: `Ism: ${values.name}:
+Telefon nomeri: ${values.tel}:
+Kurs nomi: ${values.kurs}`,
+        }),
+      }
+    )
+      .then(() => form.reset())
+      .finally(() => setIsLoading(false));
+
+    toast.promise(promise, {
+      loading: "Loading...",
+      success: "Successfully sent!",
+      error: "Something went wrong!",
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        <FormField
+          control={form.control}
+          name="tel"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="number"
+                  placeholder="Telefon nomer "
+                  disabled={isLoading}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Ismingiz" disabled={isLoading} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="kurs"
+          render={({ field }) => (
+            <FormItem>
+              {/* <FormLabel>Email</FormLabel> */}
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Kursni tanlang" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem  disabled={isLoading}  {...field}  value="Koreys tili"> Koreys tili   </SelectItem>
+                  <SelectItem disabled={isLoading} {...field}    value="Kompyuter kursi"> Kompyuter kursi  </SelectItem>
+				  <SelectItem disabled={isLoading} {...field} value="Arab tili">Arab tili</SelectItem>
+				  <SelectItem disabled={isLoading} {...field} value="Ingliz tili">Ingliz tili</SelectItem>
+				  <SelectItem disabled={isLoading} {...field} value="Web dasturlash">Web dasturlash</SelectItem>
+				  <SelectItem disabled={isLoading} {...field} value="Rus tili">Rus tili</SelectItem>
+				  <SelectItem disabled={isLoading} {...field} value="Turk tili">Turk tili</SelectItem>
+				  {/* <SelectItem disabled={isLoading} {...field} value=""> </SelectItem> */}
+
+                </SelectContent>
+              </Select>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          className="w-fit"
+          size={"lg"}
+          type="submit"
+          disabled={isLoading}
+        >
+          <span>Send</span>
+          <Send className="w-4 h-4 ml-2" />
+        </Button>
+      </form>
+    </Form>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
