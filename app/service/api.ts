e3c527693087545893c49bb5cb5d7094961adce5
Muhammad Ibrohimov <108574977+muhammadibrohimov26@users.noreply.github.com>
2025-01-24 12:00:50 +0500

    Update api.ts

--- app/service/api.ts
+++ app/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://smartmiz-2462bfd28095.herokuapp.com',
+    baseURL: 'https://smartmizbackend.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
